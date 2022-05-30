c134da9b69e96ea36c42520769175d73d0b41f5e
Misael <misagtor@gmail.com>
2022-05-29 23:50:43 -0400

    incorrecto(), orden y correccion ortografica

--- Prueba2.py
+++ Prueba2.py
@@ -1,39 +1,36 @@
-#   4- Asignación de precios de los tipos de huevos: El usuario administrador del sistema, podrá asignar los precios de cada uno de los huevos. 
-#       a. Reglas para la asignación de precio de los huevos:
-#           i. El precio mínimo de la unidad para los huevos de Gallina es de $50
-#           ii. El precio mínimo de la unidad para los huevos de Pato es de $150
-#           iii. El precio mínimo de la unidad para los huevos de Codorniz es de $50
-#           iv. El precio mínimo de la para los huevos de Avestruz es de $800
-
-#   5- Creación de un despacho: Para generar un despacho, se deben considerar los siguientes datos:
-#       b. Datos a calcular:
-#           i. Implícitamente, se deberá guardar el precio unitario del huevo, ya que el precio varía según la necesidad de la demanda.
-#           ii. Después de realizar el ingreso de la cantidad de huevos, implícitamente se deberá calcular la cantidad ingresada por precio unitario del momento del huevo.
-
+'''
+Creado por 
+Misael Garcia   18.992.359-7
+Felipe Rojas    
+Estudiantes de Ingenieria en Informatica 
+Ramo de Estructuras de datos y algoritmos
+generacion 2022 horario vespertino
+'''
 import os
 
 def limpiar ():      #funcion para limpiar pantalla, funciona en windows o linux
     os.system('cls' if os.name=='nt' else 'clear') 
 
-def esperar():      #funcion de espera, requiere una entrada, independiente del tipo
+def esperar():      #funcion de espera, requiere una entrada
     input ("\nPulse Enter para continuar...")
 
-def menu ():
+def menu ():        #funcion para la navegacion del menu, llamara las funciones correspondientes
     limpiar ()
-    print ("\nMenú All Eggs\n--------------\n1)Asignación de precios de huevos")
-    navegacion = int (input ("2)Creación de despachos\n3)Listar huevos\n4)Listar despachos\n> "))
-    if navegacion == 1 :
+    print ("\nMenú All Eggs\n--------------\n1) Asignación de precios de huevos")
+    print ("2) Creación de despachos\n3) Listar huevos\n4) Listar despachos")
+    navegacion = input ("otro)Salir\n>")
+    if navegacion == "1" :
         asignacion ()
-    elif navegacion == 2 :
+    elif navegacion == "2" :
         creacion ()
-    elif navegacion == 3 :
+    elif navegacion == "3" :
         listar ()
-    elif navegacion == 4 :
+    elif navegacion == "4" :
         listar_despachos ()
     else :
         incorrecto ()
 
-def asignacion ():       #funcion relacionada con el punto 4 del ejercicio
+def asignacion ():       #funcion para asignar valores distintos a los iniciales
     limpiar ()
     
     print ("Asignacion de precios")
@@ -41,13 +38,13 @@ def asignacion ():       #funcion relacionada con el punto 4 del ejercicio
     esperar()
     menu ()
 
-def creacion ():     #funcion relacionada con el punto 5 del jercicio
-    global ID
+def creacion ():     #funcion para crear despachos
+    global ID       #se permite la modificacion de las variables ID, valores y despachos.
     global valores
     global despachos
     limpiar ()
     ID += 1
-    rut = ""
+    rut = ""        #todos los valores de la lista se inician como "" para el control de ingreso vacio
     while rut == "":
         rut = input ("Ingrese RUT\n>")
     limpiar()
@@ -55,10 +52,10 @@ def creacion ():     #funcion relacionada con el punto 5 del jercicio
     while nombre == "":
         nombre = input ("Ingrese Nombre o Razon Social\n>")
     limpiar()
-    i = 5
+    i = 5       #se apoya en el contador i para realizar el ciclo de tipo de huevos
     while i !=  "1" and i != "2" and i != "3" and i != "4":
         limpiar()
-        i = input("Seleccione un tipo de huevo:\n1)Gallina\n2)Pato\n3)Codorniz\n4)Avestruz\n>")
+        i = input("Seleccione un tipo de huevo:\n1) Gallina\n2) Pato\n3) Codorniz\n4) Avestruz\n>")
         if i == "1":
             tipo = "Gallina"
             tipo_n = 0
@@ -74,8 +71,8 @@ def creacion ():     #funcion relacionada con el punto 5 del jercicio
         else:
             print ("Ha ingresado una opción incorrecta, intente nuevamente")
     limpiar ()
-    i = input ('si el usuariop posee convenio, ingrese "SI", de lo contrario, se asumirá que no existe convenio\n>')
-    if i == "SI" or i == "si" or i == "Si" or i == "sI":
+    i = input ('si el usuario posee convenio, ingrese "SI", de lo contrario, se asumirá que no existe convenio\n>')
+    if i == "SI" or i == "si" or i == "Si" or i == "sI":        #se acepta independiente de la forma en que lo escriba
         convenio = 1
     else:
         convenio = 0
@@ -87,41 +84,38 @@ def creacion ():     #funcion relacionada con el punto 5 del jercicio
     while 0 == 0:
         cantidad = int(input("ingrese cantidad de huevos a enviar\n>"))
         if cantidad < 50 or cantidad > 10000:
-            print("el monto debe ser mayor a 50 y menor que 10.000")
+            print("el monto debe ser mayor a 50 y menor que 10.000")        #condiciones definidas previamente
         else:
             break
     limpiar ()
     valor = 1
-    if convenio == 1:
+    if convenio == 1:       #de existir convenio, se fija el valor a un 90%
         valor = 0.9
-    valor = valor * valores[tipo_n] * cantidad
+    valor = valor * valores[tipo_n] * cantidad      #se calcula el valor total de la carga
     precio_unitario = valor/cantidad        #se almacena tomando en cuenta el convenio o solo el valor del huevo?
     lista = [rut, nombre, tipo, convenio, direccion, cantidad, precio_unitario]
-    despachos[ID] = lista
+    despachos[ID] = lista       #se almacenan los datos de la lista en el diccionario
     print ("el registro de despacho se ha realizado correctamente, con el ID ", ID)
-    try:
-        return ID
-    finally:
-        esperar()
-        menu ()
-
-def listar ():       #funcion para listar los valores de los huevos
-    limpiar ()
-    print ("Huevos de Gallina:  $", valores [0], "\nHuevos de Pato:     $",valores [1], "\nHuevos de Codorniz: $", valores [2], "\nHuevos de Avestruz: $", valores[3])
     esperar()
     menu ()
 
-def listar_despachos ():        #funcion relacionada con el punto 7 del ejercicio
-    global despachos
+def listar ():       #funcion para listar los valores de los huevos
+    limpiar ()      #la linea 104 esta dividida por el "\"
+    print ("Huevos de Gallina:  $", valores [0], "\nHuevos de Pato:     $", valores [1],\
+    "\nHuevos de Codorniz: $", valores [2], "\nHuevos de Avestruz: $", valores[3])
+    esperar()
+    menu ()
+
+def listar_despachos ():        #funcion para listar los despachos registrados
     limpiar ()
     print("Los despachos registrados son:\n")
-    for i in despachos.keys():
+    for i in despachos.keys():      #navega a traves de los keys del diccionario
         print(i)
     print()
-    consulta = int(input("ingrese el numero de despacho que desea consultar\n>1"))
+    consulta = int(input("ingrese el numero de despacho que desea consultar\n>"))
     limpiar()
     if consulta in despachos:
-        lista = despachos[consulta]
+        lista = despachos[consulta]     #se sacan los valores del diccionario, para poder trabajar con la lista
         print ("Rut:              ", lista[0])
         print ("Nombre:           ", lista[1])
         print ("Tipo de Huevo:    ", lista[2])
@@ -136,33 +130,37 @@ def listar_despachos ():        #funcion relacionada con el punto 7 del ejercici
     menu ()
 
 def incorrecto ():       #funcion que se realizara al ingresar una opcion no valida en el menu
+    global user
     limpiar ()
-    print ("opción incorrecta")
-    esperar()
-    menu ()
+    print ('para cerrar el programa ingrese la palabra "huevos"')
+    salida = input(">")     #seguridad para evitar salir por error
+    if salida == "huevos":
+        quit()
+    else:
+        menu ()
 
-valores = [50, 150, 50, 800] #gallina, pato, codorniz, avestruz
+valores = [50, 150, 50, 800]        #gallina, pato, codorniz, avestruz
 user_ = "admin"     #usuario esperado
 pwd_ = "1234"       #contraseña esperada
-i = 0     #variable para navegación en menú
-ID = 101       #Identificador de Despacho
+i = 0       #variable para navegación en menú
+ID = 101        #Identificador de Despacho
 lista = ["18992359-7", "Misael Garcia", "Gallina", "convenio", "avenida siempre viva 2785", 200, 45]
-despachos = {100:lista}
+despachos = {100:lista}     #primer valor preasignado
 lista = ["18264014-k", "Felipe Rojas", "Avestruz", "sin convenio", "Avenida siempre viva 2350", 100, 800]
-despachos[101] = lista
+despachos[101] = lista      #segundo valor preasignado
 
 print ("Bienvenido, ingrese sus datos para iniciar sesion\n")
 while 0 == 0:
-    ###
-    #user = input ("ingrese usuario: "); pwd = input ("ingrese la contraseña: ")       #se pide usuario y contraseña
-    
-    user=user_
-    pwd=pwd_
-    ###
+    user = input ("ingrese usuario: ")
+    pwd = input ("ingrese la contraseña: ")       #se pide usuario y contraseña
     if user == user_ and pwd == pwd_:
         menu ()
     else :
-        print ("\nusuario o contraseña invalido\nintento Numerno", i+1 , "de 3"); i += 1        #aumenta contador
-        esperar(); limpiar ()
+        print ("\nusuario o contraseña invalido\nintento Numerno", i+1 , "de 3")
+        i += 1        #aumenta contador
+        esperar()
+        limpiar ()
         if i == 3 :
-            print ("Ha fallado 3 veces el inicio de sesion, se cerrara el programa"); esperar(); break
+            print ("Ha fallado 3 veces el inicio de sesion, se cerrara el programa")
+            esperar()
+            break
