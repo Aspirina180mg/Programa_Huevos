42dfe8b572c32f70bd282b55631bb0e4b46d731c
Misael <misagtor@gmail.com>
2022-07-03 18:39:41 -0400

    subida

--- Prueba2.py
+++ Prueba2.py
@@ -1,72 +1,26 @@
 '''
-La empresa productora y comercializadora de huevos “All Eggs”, que está en constante expansión dentro del territorio nacional, está automatizando sus procesos administrativos. Su misión es la de Producir, comercializar y distribuir huevos de calidad. Para satisfacer la demanda dentro de estos procesos, se le ha solicitado a Usted como desarrollador en Python para dar solución, siguiendo las especificaciones que se explicitan a continuación:
-1-	All Eggs, distribuye los siguientes tipos de huevos: Gallina, Pato, Codorniz y Avestruz.
-2-	Control de Autentificación: El sistema debe contar con un control de autentificación. Es decir, al empezar el programa, deberá pedir Usuario y Contraseña. El usuario administrador inicial y contraseña los puede definir a su criterio.
-3-	Menú de opciones (post ingreso): Cuando el usuario accede al sistema, dispondrá de las siguientes opciones:
-    a.	Asignación de precios de Huevos
-    b.	Creación de despachos
-    c.	Listar huevos
-    d.	Listar despachos
-4-	Asignación de precios de los tipos de huevos: El usuario administrador del sistema, podrá asignar los precios de cada uno de los huevos. 
-    a.	Reglas para la asignación de precio de los huevos:
-        i.	El precio mínimo de la unidad para los huevos de Gallina es de $50
-        ii.	El precio mínimo de la unidad para los huevos de Pato es de $150
-        iii.	El precio mínimo de la unidad para los huevos de Codorniz es de $50
-        iv.	El precio mínimo de la para los huevos de Avestruz es de $800
 5-	Creación de un despacho: Para generar un despacho, se deben considerar los siguientes datos:
     a.	Datos a Ingresar:
-        i.	Correlativo del Despacho (ID)
-        ii.	Rut del cliente
-        iii.	Nombre o Razón Social
-        iv.	Tipo de Huevo, en donde sólo se podrá ingresar uno de los tipos registrados, que en su defecto vendría siendo: Gallina, Pato, Codorniz o Avestruz.
-        v.	¿Tiene Convenio? En dónde sólo se podrá ingresar Sí o no.
-        vi.	Dirección (domicilio) a realizar el despacho.
         vii.	Fecha Comprometida (corresponde a la fecha en que se deben enviar los huevos)
-        viii.	Cantidad de huevos a enviar.
-        ix.	Terminado el registro, éste deberá quedar almacenado, indicando que el registro se realizó correctamente. Posterior a ello, debe volver al menú.
-    b.	Datos a calcular:
-        i.	Implícitamente, se deberá guardar el precio unitario del huevo, ya que el precio varía según la necesidad de la demanda.
-        ii.	Después de realizar el ingreso de la cantidad de huevos, implícitamente se deberá calcular la cantidad ingresada por precio unitario del momento del huevo.
-    c.	Reglas a considerar:
-        i.	Al registrar un despacho, los campos no deben estar vacíos.
-        ii.	Los despachos NO SE DEBEN REPETIR (El id no debe estar duplicado) Considere que el id del despacho puede ser un número correlativo (acumulativo) en donde dicho ID será la KEY del diccionario.
-        iii.	La cantidad mínima de huevos a enviar, deberán ser 50 y máximo 10000.
-        iv.	En caso de tener convenio (Sí), el precio final del despacho tendrá un 10% de descuento.
-6-	Listar Huevos: el usuario autentificado, podrá ver todos los huevos disponibles, con sus respectivos precios unitarios.
 7-	Listar despachos: el usuario autentificado, podrá listar todos los despachos registrados en el sistema, así como también realizar búsqueda por fechas comprometidas.
 Requerimientos Adicionales
-1.	Utilizando lenguaje de programación Python, debe crear los menús necesarios para dar solución a la problemática.
-2.	Debe ser validado el ingreso de datos para no interrumpir el flujo del programa.
-3.	Determinar funciones necesarias para cada ítem de cada menú,  considerar al implementar seguir secuencia lógica necesaria para esto.
-4.	Control de autentificación inicial.
 5.	Los menús a crear son:
-    a.	Asignación de precios de Huevos
-    b.	Creación de despachos
-    c.	Listar huevos
     d.	Listar despachos con las siguientes sub-opciones:
         i.	Buscar por Fecha comprometida
         ii.	Buscar por Rut
         iii.	Listar todo
         iv.	Buscar despachos por razón social: Se deberán realizar búsquedas por la proximidad de una razón social existente. Por ejemplo:
         1.	Si tenemos registrado los despachos con las razones sociales “Comercializadora X” y “Comercio AyB Ltda.” Y yo como usuario busco por “comer”, se deberá listar los despachos de ambas razones sociales porque la palabra “comer” existe en ambas razones sociales.
-6.	Creación correcta de variables.
-7.	Modificar los huevos según reglas de negocios establecidas
-    a.	Considerar reglas de negocios apoyándose en funciones definidas que Python provee.
-8.	Ingreso correcto del despacho a realizar.
-    a.	Considerar reglas de negocios apoyándose en funciones definidas que Python provee.
-9.	Utilizar listas que permitan modificar la información de los huevos.
-10.	Utilizar un Diccionario para el registro de los despachos.
-11.	Limpieza y orden en el entregable final.
 '''
 
 '''
 Creado por
 Misael Garcia   18.992.359-7
 Felipe Rojas    18.899.270-6
-Estudiantes de Ingenieria en Informatica
+Estudiantes de Ingeniería en Informática
 Ramo de Estructuras de datos y algoritmos
 Profesor David Villegas
-generacion 2022 horario vespertino
+generación 2022 horario vespertino
 '''
 
 import os
